688238049b20cb178b9a5e24841c32f2b98cdf4b
Farhan Sangaji <farhan11soyae@gmail.com>
2025-01-17 21:34:49 +0700

    add Assignment and AssignmentCriteria models with migrations

---
+++ app/Models/Assignment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    protected $fillable = ['title'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
